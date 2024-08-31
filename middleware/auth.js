4ba08a50050dc95a5f22e44a517099c46446645d
ahmedhussein74 <ahmedhusseinofficial22@gmail.com>
2024-08-31 12:22:39 +0300

    update auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,18 +2,22 @@ const jwt = require("jsonwebtoken");
 const STATUS = require("../utils/responseStatus");
 
 const auth = (req, res, next) => {
-  const token = req.header("Authorization")?.split(" ")[1];
-  if (!token)
+  const authHeader = req.header("Authorization");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res
-      .status(STATUS.NO_CONTENT)
+      .status(STATUS.UNAUTHORIZED)
       .json({ message: "Access denied. No token provided." });
+  }
+  const token = authHeader.split(" ")[1];
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
-    req.role = decoded.role;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = {
+      id: decoded.userId,
+      role: decoded.role,
+    };
     next();
   } catch (error) {
-    res.status(STATUS.BAD_REQUEST).json({ message: "Invalid token." });
+    res.status(STATUS.FORBIDDEN).json({ message: "Invalid or expired token." });
   }
 };
 
